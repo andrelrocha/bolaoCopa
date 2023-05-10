e34eb02da7ea15e8640562ef29ab44483e52d330
andrelrocha <andrerocha0911@gmail.com>
2023-05-10 00:05:03 -0300

    feat: criando auth do google e estilização mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,9 @@
 import { NativeBaseProvider, StatusBar } from "native-base"
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
-import { SignIn } from './src/screens/SignIn'
+import { AuthContextProvider } from "./src/contexts/AuthContext"
+
+import { Find } from './src/screens/Find'
 import { Loading } from './src/components/Loading'
 
 import { THEME } from './src/styles/theme'
@@ -11,12 +13,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-        { fontsLoaded ? <SignIn/> : <Loading/> }
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+          { fontsLoaded ? <Find/> : <Loading/> }
+        </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
